598e8cfb2c2b5d6bf48f00ba1885734c9132a5c5
blacey15 <blacey@csumb.edu>
2023-02-20 15:01:29 -0800

    mario done

--- Mario Game/Assets/Platformer/Scripts/UITimer.cs
+++ Mario Game/Assets/Platformer/Scripts/UITimer.cs
@@ -6,16 +6,34 @@ using TMPro;
 public class UITimer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
+    public float timer;
+    public bool timerOn = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerOn = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        int wholeSecond = (int)Mathf.Floor(Time.realtimeSinceStartup);
-        timerText.text = wholeSecond.ToString();
+        if (timerOn)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                updateTime(ref timer);
+            }
+        }
+        //int wholeSecond = (int)Mathf.Floor(Time.realtimeSinceStartup);
+        //timerText.text = wholeSecond.ToString();
+    }
+
+    void updateTime(ref float current)
+    {
+        //current -= 1;
+        float seconds = Mathf.FloorToInt(current);
+        timerText.text = (($"{seconds}"));
     }
 }
