918fc073d090b6bfdd9122eed3a25fdb72ed5011
brielle <blacey@csumb.edu>
2023-04-07 15:35:51 -0700

    ai done, working on lighting

--- Tower Defense/Assets/Scripts/EnemyMovement.cs
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -1,32 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(Enemy))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMovement : MonoBehaviour
 {
     private Transform target;
     private int wavepointIndex = 0;
+
+    public Transform destination;
 
     private Enemy enemy;
 
     void Start()
     {
         enemy = GetComponent<Enemy>();
-        target = Waypoints.waypoints[0];
+        //target = Waypoints.waypoints[0];
     }
     
     void Update()
     {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
-        {
-            GetNextWaypoint();
-        }
-
-        enemy.speed = enemy.startSpeed;
+        GetComponent<NavMeshAgent>().SetDestination(destination.position);
+        
+        // Vector3 dir = target.position - transform.position;
+        // transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+        //
+        // if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        // {
+        //     GetNextWaypoint();
+        // }
+        //
+        // enemy.speed = enemy.startSpeed;
     }
 
     void GetNextWaypoint()
